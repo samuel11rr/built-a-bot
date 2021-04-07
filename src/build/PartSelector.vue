dd8d46b31c8e47afc8878ddaa269fa665ed3eaff
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-04-07 18:26:33 -0500

    custom directive and filter added

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,11 +3,19 @@
     <img @click="showPartInfo" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+
+    <span
+      @click="pinPadding = '30px'"
+      class="sale"
+      v-show="selectedPart.onSale"
+      v-pin:position="{ top: pinPadding, right: pinPadding }"
+    >Sale!</span>
   </div>
 </template>
 
 <script>
+// import pinDirective from '../shared/pin-directive';
+
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -19,6 +27,9 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  // directives: {
+  //   pin: pinDirective,
+  // },
   props: {
     parts: {
       type: Array,
@@ -33,7 +44,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
@@ -84,9 +98,9 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
+  /* position: absolute;
   bottom: 5px;
-  right: 5px;
+  right: 5px; */
   color: white;
   background-color: red;
   padding: 3px;
