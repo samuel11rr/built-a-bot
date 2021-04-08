d2a2a0bf2fdbf0ded9481b21648ce26ea890aa1d
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-04-07 19:01:13 -0500

    enviroment variables added

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -15,6 +15,10 @@
 <script>
 export default {
   name: 'HomePage',
+  created() {
+    console.log(process.env.VUE_APP_TEST);
+    console.log(process.env.SOME_TEST);
+  },
   props: {
     msg: String,
   },
